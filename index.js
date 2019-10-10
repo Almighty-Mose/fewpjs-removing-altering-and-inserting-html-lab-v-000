a5995460f88312a832e3fd32965469ddb57103d1
Branden Mosley <branden.mosley1337@gmail.com>
2019-10-10 19:34:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 document.getElementById('main').remove();
-let newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
+let newHeader = document.createElement('h1').setAttribute('id', 'victory').innerHTML("Branden is the champion");
+
